593f13535cbe319f40f16374b70db8f393e85e38
LuXCaesium <luxcaesium@protonmail.com>
2023-08-28 17:14:31 +0100

    add task completion validation routine

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 from tasks import add
+from celery import group
 
-result = add.delay(4, 4)
+result_group = group(add.s(i, i) for i in range(1000))()
 
-print(result.get())
+result_objects = list(result_group)
+
+processed_tasks = {}
+
+while result_objects:
+    for result in result_objects:
+        if result.ready() and result.successful():
+            df_result = result.get()
+            print(df_result)
+            processed_tasks[result.id] = df_result
+            
+    result_objects = [result for result in result_objects if result.id not in processed_tasks]
