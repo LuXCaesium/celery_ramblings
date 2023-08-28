b2f981e6f0092a7385193fb2279bfd80f7b6123f
fed-col <federicocolio1@MacBook-Pro-di-Federico.local>
2023-08-29 01:31:38 +0200

    More suitable task for parallelising

--- tasks.py
+++ tasks.py
@@ -3,5 +3,5 @@ from celery import Celery
 app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/1')
 
 @app.task
-def add(x, y):
-    return x + y
+def add(numbers):
+    return sum(numbers)
